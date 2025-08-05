cda4b2f9959d6919de87811a5937101c982465c9
Kayra <kayrabalc399@gmail.com>
2025-08-05 14:45:30 +0300

    Add printl macro.

--- elf64macros.asm
+++ elf64macros.asm
@@ -11,6 +11,25 @@ STDOUT      equ 1
     syscall
 %endmacro
 
+%macro printl 2
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mpv rsi, %1
+    mov rdx, %2
+    syscall
+
+    sub rsp, 8
+    mov byte [rsp], 0x0A
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, rsp
+    mov rdx, 1
+    syscall
+
+    add rsp, 8
+%endmacro
+
 ; SYS_EXIT // exit <value>
 %macro exit 1
     mov rax, SYS_EXIT
